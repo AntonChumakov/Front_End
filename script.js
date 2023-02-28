7b9b81385c0022542d295bc01cf8e019761dfcd8
Anton <97851889+AntonChumakov@users.noreply.github.com>
2023-02-28 20:33:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// number 1
+
+
+// const num = prompt("Vvedite chislo");
+// console.log(num / 10);
+
+
+
+// number 2
+
+
+// const num1 = prompt('Vvedite chislo')
+// const num2 = prompt('Vvedite chislo')
+// if (num1 < num2) {
+//     console.log(num1)
+// } else {
+//     console.log(num2)
+// }
+
+
+// const num3 = prompt('Number')
+// if (num3 > 50) {
+//     console.log('Bolshe')
+// } else if (num3 == 50) {
+//     console.log('Rovno')
+// } else {
+//     console.log('Menshe')
+// }
+
+
+// number 4
+
+// const myAge = prompt('Vvedite vozrast')
+// const myName = prompt('Vvedite Imya') 
+// if (myAge >= 18) {
+//     console.log('Hello ' + myName)
+// } else {
+//     console.log('Hi ' + myName)
+// }
