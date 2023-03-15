725fe11d6e678d7a38fbbb0a5f62bea1924a2fac
Anton <97851889+AntonChumakov@users.noreply.github.com>
2023-03-15 20:03:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,78 @@
-// number 1
+// ========== number 1 ==========
 
-// const numbers = [12,37,97,43,33,65,29,51,3,14]
-// for (let i = 0; i < numbers.length; i +=2) {
-//   console.log(numbers[i]);
+// function numbers(a,b) {
+//         if(a > b) {
+//             return b
+//         } else {
+//             return a
+//         }
+// }
+// console.log(numbers(23,54))
+
+// ========== number 2 ==========
+
+// function even(a,b) {
+//     for(let i = a; i >=b; i --) {
+//         if( i%2 === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// even(33,1)
+
+// ========== number 3 ==========
+
+// function power(a,b=2) {
+//     return Math.pow(a,b)
+// }
+// console.log(power(3))
+
+// ========== number 4 ==========
+
+// function num(n) {
+//     let result = 0;
+//     for(let i = 1; i <= n; i ++) {
+//         result += i
+//     }
+//     return result
+// }
+// console.log(num(10))
+
+// ========== number 5 ==========
+
+// function num(n, m) {
+//   let even = 0;
+//   let odd = 0;
+//   for (let i = n; i <= m; i++) {
+//     if (i % 2 === 0) {
+//       even += i;
+//     } else {
+//       odd += i;
+//     }
+//   }
+//   console.log(even);
+//   console.log(odd);
 // }
 
-// number 2
+// num(22, 22);
 
-// const numbers = [12,23,43,56,89,23,09,67,89,21,46,79,23,20]
-// for(let i = 0; i < numbers.length; i ++) {
-//     if(numbers[i]<55 && numbers[i]>20)
-//     console.log(numbers[i])
+
+// ========== number 6 ==========
+
+
+// function getLongestString(arr) {
+//   if (arr.length === 0) {
+//     return null;
+//   }
+//   let longestString = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i].length > longestString.length) {
+//       longestString = arr[i];
+//     }
+//   }
+//   return longestString;
 // }
+// const stringsArr = ['one', 'two', 'three'];
+// const result = getLongestString(stringsArr);
 
-// number 3
-
-// const numbers = [3, 5, 11, 2, 8, 1, 6];
-// for(let i = 0; i < numbers.length; i ++)
-// console.log(numbers[i])
-
-
-// number 4
-
-// const numbers = [3, 5, 11, 2, 8, 1, 6];
-// const numbersSquared = []
-
-// for(let i = 0; i < numbers.length; i ++) {
-// numbersSquared.push(numbers[i]*numbers[i])
-// console.log(numbersSquared[i])
-// }
-// const lastElem = numbersSquared[numbersSquared.length -1]
-// console.log(lastElem)
-
-
-// number 5
-
-
-// const user = {
-//     firstName: 'Anton',
-//     lastName: 'Chumakov',
-//     age: 32,
-//     salary: 500
-// }
-
-// console.log(`User name is ${user.firstName} ${user.lastName} He is ${user.age}`);
+// console.log(result)
