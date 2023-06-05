6448e8b3d1baefc1eb7b3bb619422e9217ca69d8
Anton <97851889+AntonChumakov@users.noreply.github.com>
2023-06-05 21:50:38 +0200

    min/pro
    hm

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-let students = [];
-
-students.push({ name: "Anton", age: 20, krus: "FE" });
-students.push({ name: "Bob", age: 30, krus: "BE" });
-students.push({ name: "Rob", age: 40, krus: "QA" });
-
-let selectStudents = students[1];
-// console.log(selectStudents);
-
-selectStudents.age = 18;
-// console.log(selectStudents.age);
-
-selectStudents.city = "Dusseldorf";
-// console.log(selectStudents);
-
-for (let i = 0; i < students.length; i++) {
-  console.log(students[i].name);
+const info = ["item", "logo", "lift", "icon", "ice", "let"];
+const prefix = info.map(item => {
+    if(item.startsWith("i")){
+        return "pro" + item
+    } else if(item.startsWith("l")) {
+    return "min" + item 
+    } else {
+        return item
+    }
 }
+)
+console.log(prefix);
