1f951754c147eee1749ca86dd712137e36d69fe8
Anton <97851889+AntonChumakov@users.noreply.github.com>
2023-06-05 21:04:33 +0200

    30.05

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-// const btn1 = document.querySelector('.btn1')
-// const square1 = document.querySelector('.square1')
-// btn1.addEventListener('click', function(){
-//     square1.classList.toggle('green')
-// })
+let students = [];
 
-// const btn2 = document.querySelector('.btn2')
-// const square2 = document.querySelector('.square2')
-// btn2.addEventListener('click', function(){
-//     square2.classList.toggle('blue')
-//     console.log(square2)
-// })
-// const btn3 = document.querySelector('.btn3')
-// const square3 = document.querySelector('.square3')
-// btn3.addEventListener('click', function(){
-//     square3.classList.toggle('yellow')
-// })
-// const btn4 = document.querySelector('.btn4')
-// const root = document.querySelector('.root')
-// btn4.addEventListener('click', function(){
-//     root.innerHTML += `<p class="p1">Privet</p>`
-// })
+students.push({ name: "Anton", age: 20, krus: "FE" });
+students.push({ name: "Bob", age: 30, krus: "BE" });
+students.push({ name: "Rob", age: 40, krus: "QA" });
 
-// const colors = document.querySelectorAll('p')
-// const btn5 = document.querySelector('.btn5')
-// const root1 = document.querySelector('.root1')
-// btn5.addEventListener('click', function(){
-//     root1.innerHTML += `<p class="p1">Privet</p><p class="p2">Privet</p>`
-// })
+let selectStudents = students[1];
+// console.log(selectStudents);
+
+selectStudents.age = 18;
+// console.log(selectStudents.age);
+
+selectStudents.city = "Dusseldorf";
+// console.log(selectStudents);
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i].name);
+}
